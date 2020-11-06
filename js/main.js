fefcd50a6145a8ec6dc86022a82e11258ba78b4f
xwaxes01 <xwaxes@gmail.com>
2020-11-06 21:58:12 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// When the user scrolls down 50px from the top of the document, resize the navbar's padding and the logo's font size
+
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("navbar-links");
+var btns = header.getElementsByClassName("navbar-links-item");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
